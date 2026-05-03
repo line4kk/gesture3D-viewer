5f7bd6f4f6bf546336c22373c9af851d045731a0
line4kk's book <silandr2016@gmail.com>
2026-05-03 21:07:11 +0400

    Переделана логика распознавателя жеста "фотоаппарат". Теперь жест распознается лучше

--- backend/src/gesture_recognizing/gesture_recognizers.py
+++ backend/src/gesture_recognizing/gesture_recognizers.py
@@ -37,9 +37,33 @@ def is_photo_gesture(rcg_result: GestureRecognizerResult):
         return False
     if not is_thumb_normal_to(hand1_landmarks, np.array([1, 0, 0])) or not is_thumb_normal_to(hand2_landmarks, np.array([1, 0, 0])):
         return False
-    if not is_tips_close(rcg_result.hand_landmarks, 2):
+
+    index1 = get_finger_vector(hand1_landmarks, 1)
+    thumb1 = get_thumb_vector(hand1_landmarks)
+    index2 = get_finger_vector(hand2_landmarks, 1)
+    thumb2 = get_thumb_vector(hand2_landmarks)
+
+    direction1 = index1 + thumb1
+    direction2 = index2 + thumb2
+
+    # Арктангенсы получаются отрицательными, т.к. в Mediapipe ось OY идет от верха экрана к низу
+    arctg1 = -np.arctan2(direction1[1], direction1[0])
+    arctg2 = -np.arctan2(direction2[1], direction2[0])
+    if (arctg1 * arctg2) > 0:
         return False
 
+    top_hand_index, bottom_hand_ind = sorted([0, 1], key=lambda x: [arctg1, arctg2][x])
+
+    # Проверяем, что кончик указательного пальца верхней руки находится на кадре выше, чем кончик большого пальца нижней руки
+    if rcg_result.hand_landmarks[top_hand_index][8].y > rcg_result.hand_landmarks[bottom_hand_ind][4].y:
+        return False
+
+    bottom_direction = (direction1, direction2)[bottom_hand_ind]
+    vector_between_hands = to_np_vector(landmark_to_xOy_projection(rcg_result.hand_landmarks[bottom_hand_ind][1]),
+                                        landmark_to_xOy_projection(rcg_result.hand_landmarks[top_hand_index][1]))
+
+    if not is_codirectional(bottom_direction, vector_between_hands, 25):
+        return False
 
     return True
 
