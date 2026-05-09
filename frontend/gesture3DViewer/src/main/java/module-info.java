21c282eb7ad8d41482f2704735ffd03ce615021d
line4kk's book <silandr2016@gmail.com>
2026-05-09 16:38:46 +0400

    Добавлена библиотека Assimp для работы с файлами 3D сцен

--- frontend/gesture3DViewer/src/main/java/module-info.java
+++ frontend/gesture3DViewer/src/main/java/module-info.java
@@ -4,11 +4,13 @@ module com.line4kk.gesture3dviewer {
     requires jeromq;
     requires com.fasterxml.jackson.databind;
     requires jdk.compiler;
+    requires org.lwjgl.assimp;
+    requires javafx.graphics;
 
 
-    opens com.line4kk.gesture3dviewer to javafx.fxml;
     exports com.line4kk.gesture3dviewer;
     exports com.line4kk.gesture3dviewer.model;
 
     opens com.line4kk.gesture3dviewer.model to com.fasterxml.jackson.databind;
+    opens com.line4kk.gesture3dviewer to com.fasterxml.jackson.databind, javafx.fxml;
 }
